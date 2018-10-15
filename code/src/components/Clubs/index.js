05686a29c95467afc79472299f2c6f261095e489
Becki Wordsworth <r.a.wordsworth@gmail.com>
2018-10-15 17:04:56 +0200

    Initial Club Filter Function

---
+++ code/src/components/Clubs/index.js
@@ -0,0 +1,54 @@
+import React from "react"
+
+let URL = `https://api.www.svenskaspel.se/player/sponsorship/autocomplete?search=${query}&numResponses={integer}`
+let query =  ""
+
+class Clubs extends React.Component {
+
+
+
+  constructor(props) {
+      super(props)
+      this.state = {
+        clubs: ""
+      }
+    }
+
+    onFilterTextChange = (evt) => {
+    this.query = evt.target.value;
+    this.forceUpdate();
+  }
+
+
+    componentDidMount() {
+      fetch(URL).then((response) => {
+        return response.json()
+      }).then((json) => {
+        this.setState({
+          clubs: json.data
+        })
+      })
+    }
+
+
+
+
+
+
+  render() {
+
+
+    return (
+      <div>
+
+      <input type="text" onChange={this.onFilterTextChange} /> 
+
+
+
+      </div>
+    )
+  }
+
+}
+
+export default Clubs
