577d4efe6f53d2097169c1612f1504d8b89d319d
Becki Wordsworth <r.a.wordsworth@gmail.com>
2018-10-17 14:49:28 +0200

    Made the API work.
    On click of the button query is defined and passed into the API to call.
    
    NB Don't use submit because it has its own properties and it re-renders the page. If you use it use event.preventDefault()

--- code/src/components/Clubs/index.js
+++ code/src/components/Clubs/index.js
@@ -1,59 +1,57 @@
 import React from "react"
 
-let URL = `https://api.www.svenskaspel.se/player/sponsorship/autocomplete?search=${query}&numResponses={integer}`
-let query = this.state.query
+
 
 class Clubs extends React.Component {
-
-
 
   constructor(props) {
       super(props)
       this.state = {
-        clubs: ""
+        clubs: [],
+        query: ""
       }
     }
 
-    handleInputChange = (evt) => {
-    this.query = evt.target.value;
-    this.forceUpdate();
-  }
+    handleClick = (evt) => {
+//this.setState({query: evt.target.value})
+      console.log(this.state.query)
+      evt.preventDefault()
 
+      let searchTerm = this.state.query
 
-    componentDidMount() {
-      fetch(URL).then((response) => {
-        return response.json()
-      }).then((json) => {
-        this.setState({
-          clubs: json.data
+      if (searchTerm === "") {
+        alert("That was an invalid search, try again")
+      } else {
+        const url = `https://api.www.svenskaspel.se/player/sponsorship/autocomplete?search=${searchTerm}&numResponses=100`
+        fetch(url).then((response) => {
+          return response.json()
+        }).then((json) => {
+          this.setState({
+            clubs: json.data
+          })
+          console.log(json.data)
         })
-      })
-    }
+      }}
 
 
-
+  queryChange = (evt) => {
+    //console.log("hell0")
+   this.setState({query: evt.target.value})
+ }
 
 
 
   render() {
-
-
     return (
       <div>
 
       <form>
-             <input
-               placeholder="Search for..."
-               ref={input => this.search = input}
-               onChange={this.handleInputChange}
-             />
-             <p>{this.state.query}</p>
+      <input id="site-search" type="search" placeholder="Search for..." value={this.state.query} onChange={this.queryChange} />
+      <input type="submit" value="Search" onClick={this.handleClick} />
            </form>
-
       </div>
     )
   }
-
 }
 
 export default Clubs
