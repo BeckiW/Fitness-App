b9498a3c1fb7db5081aed3f1d4defb24cb029515
kblanchfield <kateblanchfield@live.co.uk>
2018-10-17 14:49:00 +0200

    Add user selected data to a global array of activities

--- code/src/components/activity/index.js
+++ code/src/components/activity/index.js
@@ -3,56 +3,66 @@ import DayPicker from 'react-day-picker';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
 import 'react-day-picker/lib/style.css'
 
+const activity = ["swim", "run", "cycle", "hike", "tennis", "team"]
+const duration = ["30 mins", "1 hour", "2 hours", "4 hours", "8 hours"]
+
+let activityList = []
+let activityItem = {}
+
 class Activity extends React.Component {
 
   constructor(props) {
       super(props)
       this.state = {
-        presets: {
-          activity: ["swim", "run", "cycle", "hike", "tennis", "team"],
-          duration: ["30 mins", "1 hour", "2 hours", "4 hours", "8 hours"]
-        },
-        entryLog: []
+        selectedActivity: "",
+        selectedDuration: "",
+        selectedDate: ""
       }
     }
 
-
     addEntry = (day) => {
       this.setState({
-        entryLog: this.state.entryLog.push(day)
+        selectedDate: day.toString()
       })
-      console.log(this.state.entryLog)
     }
 
-
+    handleClick = () => {
+      activityItem = {
+        activity: this.state.selectedActivity,
+        duration: this.state.selectedDuration,
+        date: this.state.selectedDate
+      }
+      activityList.push(activityItem)
+      console.log(activityList)
+    }
 
   render() {
     return (
       <div>
 
-      <p>Activity</p>
-      <select>
-        {this.state.presets.activity.map((activity) => {
-          return (<option key={activity} value={activity}>{activity}</option>
-          )
-        })}
-      </select>
+        <p>Activity</p>
+        <select onChange={event => this.setState({selectedActivity: event.target.value})}>
+          {activity.map((activity) => {
+            return (<option key={activity} value={activity}>{activity}</option>
+            )
+          })}
+        </select>
 
-      <p>Duration</p>
-      <select>
-        {this.state.presets.duration.map((duration) => {
-          return (<option key={duration} value={duration}>{duration}</option>
-          )
-        })}
-      </select>
+        <p>Duration</p>
+        <select onChange={event => this.setState({selectedDuration: event.target.value})}>
+          {duration.map((duration) => {
+            return (<option key={duration} value={duration}>{duration}</option>
+            )
+          })}
+        </select>
 
-      <p>Date</p>
-      <DayPickerInput onDayChange={day => this.addEntry(day)} />
+        <p>Date</p>
+        <DayPickerInput onDayChange={day => this.addEntry(day)} />
 
+        <button type="button" onClick={this.handleClick}>Add</button>
       </div>
     )
   }
-
 }
 
 export default Activity
