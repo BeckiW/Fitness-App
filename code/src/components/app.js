7cdcd2a77a1d9979385c712cbda199ac0bec13d8
Becki Wordsworth <r.a.wordsworth@gmail.com>
2018-10-22 11:42:52 +0200

    Updated router and fixed merge conflicts

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -7,23 +7,77 @@ import Activity from "./activity"
 
 class App extends React.Component {
 
-  render() {
-    return (
+  activityList = ["swim", "hike", "gym"]
+  durationList = ["30 mins", "1 hour", "2 hours", "4 hours", "8 hours"]
 
-      <Router>
-
-      <Switch>
-          <Route exact path="/" component={Clubs} />
-          <Route exact path="/activity" component={Activity}/>
-          <Route exact path="/calender" component={Calendar}/>
-          <Route path="/list" component={Clubs}/>
-        </Switch>
-
-      </Router>
-
-    )
+  state = {
+      data: [],
+      swimData: [],
+      hikeData: [],
+      gymData: []
   }
 
+  addEntry = (selectedEntry) => {
+    const data = this.state.data
+    data.push(selectedEntry)
+    this.setState({
+      data
+    }, this.filterData)
+  }
+
+
+  filterData = () => {
+    const swimData = this.state.data.filter((activity) => (
+      activity.selectedActivity === "swim"
+    )).map((activity) => (
+      activity.selectedDate
+    ))
+    const hikeData = this.state.data.filter((activity) => (
+      activity.selectedActivity === "hike"
+    )).map((activity) => (
+      activity.selectedDate
+    ))
+    const gymData = this.state.data.filter((activity) => (
+      activity.selectedActivity === "gym"
+    )).map((activity) => (
+      activity.selectedDate
+    ))
+    this.setState({
+      swimData,
+      hikeData,
+      gymData
+    })
+  }
+
+  render() {
+    return (
+        <Router>
+        <div>
+          <Link to="/activity">Add an activity</Link>
+          <br />
+          <Link to="/calendar">View your calendar</Link>
+          <Switch>
+              <Route exact path="/" component={Clubs} />
+              <Route exact
+                path="/activity"
+                render={(props) => <Activity {...props}
+                  activityList={this.activityList}
+                  durationList={this.durationList}
+                  onClick={this.addEntry} />}
+              />
+              <Route exact path="/calendar"
+                render={(props) => <Calendar {...props}
+                  data={this.state.data}
+                  swimData={this.state.swimData}
+                  hikeData={this.state.hikeData}
+                  gymData={this.state.gymData} />}
+                />
+              <Route path="/list" component={ClubList}/>
+            </Switch>
+          </div>
+        </Router>
+    )
+  }
 }
 
 export default App
